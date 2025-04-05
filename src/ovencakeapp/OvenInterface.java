3753b3bfcd45f4fd490d24448d66704997a56697
Kml251 <2024455@student.cct.ie>
2025-04-05 01:25:14 +0100

    Adding Interface

--- src/ovencakeapp/OvenInterface.java
+++ src/ovencakeapp/OvenInterface.java
@@ -4,11 +4,29 @@ package ovencakeapp;
  *
  * @author kamil
  */
+/**
+ * OvenInterface defines the structure for any oven-like data structure.
+ * It represents a collection that supports typical Queue operations:
+ * adding, removing, checking the first item, etc.
+ * This interface allows different implementations (like MyOven or MyOvenStack)
+ * while keeping the core methods consistent across them. 
+ * In this project, MyOven implements this interface using FIFO(First In First Out) logic.
+ */
+
 public interface OvenInterface {
+    //To check if the oven (queue) is empty.
     boolean isEmpty();
+    
+    //To return the number of cakes currently in the oven.
     int size();
+    
+    //To retrieve (without removing) the first cake in the oven.
     Object frontElement();
+    
+    //To add a new cake to the oven.
     void enqueue (Object element);
+    
+    //To remove the first (oldest) cake from the oven.
     Object dequeue();
     
 }
