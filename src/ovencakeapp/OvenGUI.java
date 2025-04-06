488a6d5fd5c79fa6633303591ed2083414fdabb3
Kml251 <2024455@student.cct.ie>
2025-04-06 04:56:45 +0100

    Setting the "Add" button.

--- src/ovencakeapp/OvenGUI.java
+++ src/ovencakeapp/OvenGUI.java
@@ -4,11 +4,14 @@
  */
 package ovencakeapp;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author kamil
  */
 public class OvenGUI extends javax.swing.JFrame {
+    private final MyOven oven = new MyOven();
 
     /**
      * Creates new form OvenGUI
@@ -43,6 +46,7 @@ public class OvenGUI extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jButton1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jButton1.setText("Exit");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -54,10 +58,18 @@ public class OvenGUI extends javax.swing.JFrame {
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
 
+        jButton2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jButton2.setText("Add");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
+        jButton3.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jButton3.setText("Seek the Cake");
 
+        jButton4.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jButton4.setText("List All Cakes");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -65,6 +77,7 @@ public class OvenGUI extends javax.swing.JFrame {
             }
         });
 
+        jButton5.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jButton5.setText("Remove");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -72,7 +85,8 @@ public class OvenGUI extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setText("jLabel1");
+        jLabel1.setFont(new java.awt.Font("Serif", 1, 18)); // NOI18N
+        jLabel1.setText("Welcome to 2024455_Cakes Bakery");
 
         jLabel2.setText("Cakes's Name :");
 
@@ -92,11 +106,12 @@ public class OvenGUI extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(287, 287, 287))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(223, 223, 223)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(35, 35, 35)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -118,21 +133,19 @@ public class OvenGUI extends javax.swing.JFrame {
                                         .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
                                     .addComponent(jTextField1)
                                     .addComponent(jTextField2)
-                                    .addComponent(jTextField3))))))
+                                    .addComponent(jTextField3)))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(171, 171, 171)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(38, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(287, 287, 287))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(17, 17, 17)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
                             .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -143,7 +156,7 @@ public class OvenGUI extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel4)
                     .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2)
                     .addComponent(jButton3)
@@ -174,6 +187,51 @@ public class OvenGUI extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         System.exit(0);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        //To introduce the fields as a String: getText() is to add value into the cake name; trim() is to clean any space around the value to add by mistake.
+        String name = jTextField1.getText().trim(); //To get cake's name
+        String weightStr = jTextField2.getText().trim();//To get cake's weight as a String
+        String dateStr = jTextField3.getText().trim();//To get cake's expiration date as a String
+        
+        //To check all fields are filled in.
+        if (name.isEmpty() || weightStr.isEmpty() || dateStr.isEmpty()){
+            jTextArea1.setText("Please fill in all fields. \n");
+            return;
+        }
+        //To try-catch is getting any values without numbers.
+        try{
+            int weight = Integer.parseInt(weightStr);
+            LocalDate bestBefore =LocalDate.parse(dateStr);
+            LocalDate today = LocalDate.now();
+            
+            //To check if the oven is full
+            if (oven.size() >= 5){
+                
+                jTextArea1.setText("No more capacity in the Oven! Please remove the first cake! \n");
+            }
+            else {
+                Cakes cake = new Cakes(name, weight, bestBefore);
+                oven.enqueue(cake);
+                jTextArea1.setText("Cake added: " + cake.toString());
+                
+            }
+            //To check if the expiration date is within 14 days
+            if(bestBefore.isAfter(today.plusDays(14))){
+                jTextArea1.setText("Best-before date must be within 14 days from today.\n");
+                return;
+            }
+            //To clean fields after adding the date
+            jTextField1.setText(" ");
+            jTextField2.setText(" "); /* I have only chosen name and weight because expiration days cannot be changed every time! */
+                       
+        } catch (NumberFormatException e){
+            jTextArea1.setText("Weight must be a number. \n");
+        } catch (Exception e){
+            jTextArea1.setText("Invalid data format. Use YYYY-MM-DD. \n");
+        }
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
