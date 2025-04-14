51cc21df4ad5b5e37ee5726baf846d8cded3498f
Kml251 <2024455@student.cct.ie>
2025-04-14 16:28:19 +0100

    Final bug fixes and formatting cleanup.

--- src/ovencakeapp/OvenCakeApp.java
+++ src/ovencakeapp/OvenCakeApp.java
@@ -10,7 +10,7 @@ public class OvenCakeApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //To set visible GUI screen
+        // To set visible GUI screen
         OvenGUI ovenApp = new OvenGUI();
         ovenApp.setVisible(true);
     }
